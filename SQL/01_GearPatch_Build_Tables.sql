f33be2852cb5c839bcdcf7198b00eab25f8bdf69
David Larsen <davidplarsen@gmail.com>
2020-10-22 18:07:05 -0500

    SQL Database build script

---
+++ SQL/01_GearPatch_Build_Tables.sql
@@ -0,0 +1,164 @@
+USE [master]
+
+IF db_id('GearPatch') IS NULl
+  CREATE DATABASE [GearPatch]
+GO
+
+USE [GearPatch]
+GO
+
+
+DROP TABLE IF EXISTS [UserProfile];
+DROP TABLE IF EXISTS [Gear];
+DROP TABLE IF EXISTS [GearType];
+DROP TABLE IF EXISTS [GearPics];
+DROP TABLE IF EXISTS [GearReview];
+DROP TABLE IF EXISTS [OwnerReview];
+DROP TABLE IF EXISTS [CustomerReview];
+DROP TABLE IF EXISTS [Accessory];
+DROP TABLE IF EXISTS [Reservation];
+DROP TABLE IF EXISTS [Message];
+GO
+
+
+
+CREATE TABLE [UserProfile] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [FirstName] nvarchar(40) NOT NULL,
+  [LastName] nvarchar(40) NOT NULL,
+  [Email] nvarchar(50) NOT NULL,
+  [Phone] nvarchar(20) NOT NULL,
+  [FirebaseId] nvarchar(28) NOT NULL,
+  [ImageLocation] nvarchar,
+  [IsActive] bit NOT NULL,
+
+  CONSTRAINT UQ_FirebaseId UNIQUE(FirebaseId)
+)
+
+
+CREATE TABLE [Gear] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [UserProfileId] integer NOT NULL,
+  [Headline] nvarchar(50) NOT NULL,
+  [Manufacturer] nvarchar(40) NOT NULL,
+  [Model] nvarchar(40) NOT NULL,
+  [Notes] nvarchar(max),
+  [Price] integer NOT NULL,
+  [IsActive] bit NOT NULL,
+  [TypeId] integer NOT NULL,
+  [FirstOptionNotes] nvarchar(255),
+  [SecondOptionNotes] nvarchar(255),
+
+  CONSTRAINT [FK_Gear_UserProfile] FOREIGN KEY ([UserProfileId]) REFERENCES [UserProfile] ([Id]),
+  CONSTRAINT [FK_Gear_Type] FOREIGN KEY ([TypeId]) REFERENCES [Type] ([Id])
+)
+
+
+CREATE TABLE [GearType] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [Name] nvarchar(50) NOT NULL,
+  [FirstOptionName] nvarchar(50),
+  [SecondOptionName] nvarchar(50)
+)
+
+
+CREATE TABLE [GearPics] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [GearId] integer NOT NULL,
+  [ImageLocation] nvarchar NOT NULL
+)
+
+
+CREATE TABLE [GearReview] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [GearId] integer NOT NULL,
+  [ReservationId] integer NOT NULL,
+  [Rating] integer NOT NULL,
+  [Details] nvarchar(500),
+
+  CONSTRAINT [FK_GearReview_Gear] FOREIGN KEY ([GearId])
+	REFERENCES [Gear] ([Id]),
+
+  CONSTRAINT [FK_GearReview_Reservation] FOREIGN KEY ([ReservationId])
+	REFERENCES [Reservation] ([Id]),
+
+)
+
+
+CREATE TABLE [OwnerReview] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [UserProfileId] integer NOT NULL,
+  [ReservationId] integer NOT NULL,
+  [Rating] integer NOT NULL,
+  [Details] nvarchar(500),
+    
+  CONSTRAINT [FK_OwnerReview_UserProfile] FOREIGN KEY ([UserProfileId])
+	REFERENCES [UserProfile] ([Id]),
+
+  CONSTRAINT [FK_OwnerReview_Reservation] FOREIGN KEY ([ReservationId])
+	REFERENCES [Reservation] ([Id]),
+
+)
+
+
+CREATE TABLE [CustomerReview] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [UserProfileId] integer NOT NULL,
+  [ReservationId] integer NOT NULL,
+  [Rating] integer NOT NULL,
+  [Details] nvarchar(500),
+    
+  CONSTRAINT [FK_CustomerReview] FOREIGN KEY ([UserProfiled])
+	REFERENCES [UserProfile] ([Id]),
+
+  CONSTRAINT [FK_CustomerReview_Reservation] FOREIGN KEY ([ReservationId])
+	REFERENCES [Reservation] ([Id]),
+
+)
+
+
+CREATE TABLE [Accessory] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [GearId] integer NOT NULL,
+  [Name] nvarchar(50) NOT NULL,
+  [Notes] nvarchar(500),
+
+  CONSTRAINT [FK_Accessory_Gear] FOREIGN KEY ([GearId]) REFERENCES [Gear] ([Id])
+)
+
+
+CREATE TABLE [Reservation] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [OwnerId] integer NOT NULL,
+  [CustomerId] integer NOT NULL,
+  [GearId] integer NOT NULL,
+  [AgreedPrice] integer NOT NULL,
+  [StartDate] datetime NOT NULL,
+  [EndDate] datetime NOT NULL,
+  [Confirmed] bit NOT NULL,
+  [ItemReturned] bit NOT NULL,
+
+  CONSTRAINT [FK_Reservation_UserProfile_Owner] FOREIGN KEY ([OwnerId])
+	REFERENCES [UserProfile] ([Id]),
+
+  CONSTRAINT [FK_Reservation_UserProfile_Customer] FOREIGN KEY ([CustomerId])
+	REFERENCES [UserProfile] ([Id]),
+
+  CONSTRAINT [FK_Reservation_Gear] FOREIGN KEY ([GearId]) REFERENCES [Gear] ([Id])
+)
+
+
+CREATE TABLE [Message] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [SenderId] integer NOT NULL,
+  [RecipientId] integer NOT NULL,
+  [Unread] bit NOT NULL,
+  [Text] nvarchar(1000) NOT NULL,
+
+  CONSTRAINT [FK_Message_UserProfile_Sender] FOREIGN KEY ([SenderId])
+	REFERENCES [UserProfile] ([Id]),
+
+  CONSTRAINT [FK_Reservation_UserProfile_Recipient] FOREIGN KEY ([RecipientId])
+	REFERENCES [UserProfile] ([Id])
+)
+GO
