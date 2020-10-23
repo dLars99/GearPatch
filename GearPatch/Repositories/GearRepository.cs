f91e965d3cf84d526f9a6f18c462e83b883e29d9
David Larsen <davidplarsen@gmail.com>
2020-10-22 20:55:24 -0500

    Initialize Gear and UserProfile classes

---
+++ GearPatch/Repositories/GearRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GearPatch.Repositories
+{
+    public class GearRepository
+    {
+    }
+}
