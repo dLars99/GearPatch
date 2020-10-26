3ebd3a8585102386bdb60bade7047281ccb7c718
David Larsen <davidplarsen@gmail.com>
2020-10-25 20:38:12 -0500

    Authorize UserProfile routes

--- GearPatch/Controllers/UserProfileController.cs
+++ GearPatch/Controllers/UserProfileController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace GearPatch.Controllers
 {
-    //[Authorize]
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class UserProfileController : ControllerBase
