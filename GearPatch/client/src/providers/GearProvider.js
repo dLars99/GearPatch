c9f1d8e464b191e912b46b80e1a0675598acc8ab
David Larsen <davidplarsen@gmail.com>
2020-10-29 20:04:02 -0500

    Render edit block

--- GearPatch/client/src/providers/GearProvider.js
+++ GearPatch/client/src/providers/GearProvider.js
@@ -65,6 +65,25 @@ export function GearProvider(props) {
         }
     }
 
+    const saveEditedGear = async (gear) => {
+        const token = await getToken();
+        const res = await fetch(`url/${gear.id}`, {
+            method: "PUT",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(gear)
+        });
+        const data = await res.json();
+
+        if (res.ok) {
+            return data;
+        } else {
+            alert("An error has occurred. Please try again.");
+        }
+    }
+
     const getGearTypes = async () => {
         const token = await getToken();
         const res = await fetch("/api/geartype", {
@@ -93,7 +112,7 @@ export function GearProvider(props) {
 
     return (
         <GearContext.Provider value={{ gear, searchGear, getGearItem, getMore,
-            saveNewGear, getGearTypes, getMyGear }}>
+            saveNewGear, getGearTypes, getMyGear, saveEditedGear }}>
             {props.children}
         </GearContext.Provider>
     )
