b75d79300fc60dfacac4283ae6e09130009899c2
David Larsen <davidplarsen@gmail.com>
2020-11-02 14:48:47 -0600

    Create user image upload in API

--- GearPatch/Controllers/ImageController.cs
+++ GearPatch/Controllers/ImageController.cs
@@ -50,6 +50,38 @@ namespace Tabloid.Controllers
             return Ok();
         }
 
+        [HttpPost("user")]
+        public IActionResult UploadUserImage(IFormFile file)
+        {
+            //where images are stored
+            var savedImagePath = Path.Combine(_webhost.WebRootPath, "user-images", file.FileName);
+            try
+            {
+                using var image = Image.Load(file.OpenReadStream());
+
+                int originalWidth = image.Width;
+                int originalHeight = image.Height;
+
+
+                int maxWidth = 640;
+                if (originalWidth > maxWidth)
+                {
+                    int newHeight = maxWidth * originalHeight;
+                    newHeight /= originalWidth;
+
+                    image.Mutate(i => i.Resize(maxWidth, newHeight));
+                }
+
+                image.Save(savedImagePath);
+            }
+            catch
+            {
+                return Conflict();
+            }
+
+            return Ok();
+        }
+
         [HttpGet("gear/{imageUrl}")]
         public IActionResult GetGearImage(string imageUrl)
         {
